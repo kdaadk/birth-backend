cc7a92f72827b2fc142677845858c1715fe11908
Dmitro <35249370+kdaadk@users.noreply.github.com>
2020-03-06 13:35:02 +0300

    Update app.ts
    change cors policy

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,11 @@ class App {
 
     private initializeMiddlewares() {
         this.app.use(bodyParser.json());
-        this.app.use(cors());
+        this.app.use(function(req, res, next) {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          next();
+        });
     }
 
     private initializeControllers(controllers: BaseController[]) {
